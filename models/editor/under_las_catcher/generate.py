8de189b1728a95d9d585749c4277f36bfed940d9
TeamSpen210 <spencerb21@live.com>
2015-09-27 18:19:39 +1000

    Make laser catcher models animate

--- models/editor/under_las_catcher/generate.py
+++ models/editor/under_las_catcher/generate.py
@@ -7,14 +7,15 @@ header = """\
 version 1
 nodes
 0 "base"       -1
-1 "side_nnw"   0
-2 "side_nww"   0
-3 "side_nne"   0
-4 "side_nee"   0
-5 "side_ssw"   0
-6 "side_sww"   0
-7 "side_sse"   0
-8 "side_see"   0
+9 "rotation"   -1
+1 "side_nnw"   9
+2 "side_nww"   9
+3 "side_nne"   9
+4 "side_nee"   9
+5 "side_ssw"   9
+6 "side_sww"   9
+7 "side_sse"   9
+8 "side_see"   9
 end
 skeleton
 """
@@ -23,18 +24,37 @@ frame_temp = """\
 time {time}
  0  0 0 0\t0 0 0
 """
+rot_temp = """\
+ 9  0 0 {z}\t0 0 {rot}
+"""
+bone_temp = """\
+ {bone} {x} {y} 0\t0 0 0
+"""
 DIST = [0, 1, 3] + list(range(6, 40, 6))
+VERT = [0, 1, 2] + list(range(4, 28, 4))
 
-for file, distance in [
-        ('anim_expand.smd', DIST),
-        ('anim_contract.smd', DIST[::-1]),
-        ('anim_out.smd', DIST[-1:]),
-        ('anim_idle.smd', DIST[:1]),
+assert len(DIST) == len(VERT)
+
+for file, horiz, vert in [
+        ('anim_expand.smd', DIST, VERT),
+        ('anim_contract.smd', DIST[::-1], VERT[::-1]),
+        ('anim_out.smd', DIST[-1:], VERT[-1:]),
+        ('anim_idle.smd', DIST[:1], VERT[:1]),
         ]:
     print('Making', file)
     with open(file, 'w') as f:
         f.write(header)
-        for time, dist in enumerate(distance):
-            f.write(frame_temp.format(time))
-            for bone, ang in enumerate([
+        for time, (f_hor, f_ver) in enumerate(zip(horiz, vert)):
+            f.write(frame_temp.format(time=time))
+            for bone, ang in enumerate([7, 6, 0, 1, 4, 5, 2, 3], start=1):
+                ang = (ang+0.5)/4 * math.pi
+                f.write(bone_temp.format(
+                    bone=bone,
+                    x=round(f_hor * math.sin(ang), 7),
+                    y=round(f_hor * math.cos(ang), 7),
+                ))
+            f.write(rot_temp.format(
+                z=f_ver,
+                rot=round(math.radians(f_ver*4), 7),
+            ))
         f.write("end")
