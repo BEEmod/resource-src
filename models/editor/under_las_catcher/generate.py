75faf1ca236a89cec05bb98201ae94a7a651b22b
TeamSpen210 <spencerb21@live.com>
2015-09-28 14:21:59 +1000

    Improve laser catcher animation
    - Fix position of arrows
    - Make animation staggered slightly
    - For offset model, ensure sides don't collide with walls

--- models/editor/under_las_catcher/generate.py
+++ models/editor/under_las_catcher/generate.py
@@ -30,28 +30,40 @@ rot_temp = """\
 bone_temp = """\
  {bone} {x} {y} 0\t0 0 0
 """
-DIST = [0, 1, 3] + list(range(6, 40, 6))
-VERT = [0, 1, 2] + list(range(4, 28, 4))
+DIST = [0, 1, 3] + list(range(6, 40*2, 6))
+VERT = [0, 1, 2] + list(range(4, 28, 4)) + [28] * 7
 
 assert len(DIST) == len(VERT)
 
-for file, horiz, vert in [
-        ('anim_expand.smd', DIST, VERT),
-        ('anim_contract.smd', DIST[::-1], VERT[::-1]),
-        ('anim_out.smd', DIST[-1:], VERT[-1:]),
-        ('anim_idle.smd', DIST[:1], VERT[:1]),
+for file, is_off, horiz, vert, in [
+        ('anim_expand_cent.smd', False, DIST, VERT),
+        ('anim_contract_cent.smd', False, DIST[::-1], VERT[::-1]),
+        ('anim_out_cent.smd', False, DIST[-1:], VERT[-1:]),
+        ('anim_expand_off.smd', True, DIST, VERT),
+        ('anim_contract_off.smd', True, DIST[::-1], VERT[::-1]),
+        ('anim_out_off.smd', True, DIST[-1:], VERT[-1:]),
         ]:
     print('Making', file)
     with open(file, 'w') as f:
         f.write(header)
-        for time, (f_hor, f_ver) in enumerate(zip(horiz, vert)):
+        for time, (f_horiz, f_ver) in enumerate(zip(horiz, vert)):
             f.write(frame_temp.format(time=time))
+                
             for bone, ang in enumerate([7, 6, 0, 1, 4, 5, 2, 3], start=1):
+                # Subtract the value of 'angle' to stagger the movement a bit
+                dist = max(f_horiz - ang, 0)
+                # For the offset item, we want to limit the extension of
+                # some plates to stop them going through walls
+                dist = min(dist, 
+                   12 
+                   if (bone in (4, 7, 8) and is_off) 
+                   else 40
+                )
                 ang = (ang+0.5)/4 * math.pi
                 f.write(bone_temp.format(
                     bone=bone,
-                    x=round(f_hor * math.sin(ang), 7),
-                    y=round(f_hor * math.cos(ang), 7),
+                    x=round(dist * math.sin(ang), 7),
+                    y=round(dist * math.cos(ang), 7),
                 ))
             f.write(rot_temp.format(
                 z=f_ver,
