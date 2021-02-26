25e108f7d978d49869c0c8e28852db66bd393c68
TeamSpen210 <spencerb21@live.com>
2021-02-26 13:36:59 +1000

    Make rotation of models actually work

--- models/editor/fizzler_shapes/gen_shapes.py
+++ models/editor/fizzler_shapes/gen_shapes.py
@@ -1,27 +1,33 @@
 """Generate models for all the different fizzler sizes."""
 from typing import NamedTuple, List, Tuple, Dict
 from srctools.smd import Mesh, Bone, BoneFrame, Triangle, Vertex
-from srctools import VMF, Vec
+from srctools import VMF, Vec, Angle
 import os
+import subprocess
 from math import radians
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 
 # Name to use for the root bone.
 ROOT_NAME = 'root'
 CACHE = {}
 
 STYLES = [
-    ('clean', 'fizzler/fizz_effect_portal'),
-    ('retro', 'fizzler/fizz_effect_retro_portal'),
+    ('clean', Angle(0, 0, 0), 'fizzler/fizz_effect_portal'),
+    ('retro', Angle(0, 90, 0), 'fizzler/fizz_effect_retro_portal'),
 ]
 
+P2_LOC = os.environ.get('PORTAL_2_LOC')
+print('Portal 2: ', P2_LOC)
 
 class Shape:
     """A fizzler size, plus the block geo."""
-    def __init__(self, name: str, block: str, points: List[Tuple[Vec, Vec]]) -> None:
+    def __init__(self, name: str, block: str, points: List[Tuple[Vec, Angle]]) -> None:
         self.name = name
         self.points = points
         self.block_fname = block + '.smd'
+
+    def __repr__(self) -> str:
+        return f'<Shape {self.name}>'
 
 
 SHAPES = [
@@ -29,7 +35,7 @@ SHAPES = [
         f'standing_fizzler_x{i}',
         f'blocks/standing_{i}_ref',
         [
-            (Vec(128*off, 0, 0), Vec(90, 0, yaw))
+            (Vec(128*off, 0, 0), Angle(90, 0, yaw))
             for off in range(5)[:i]
             for yaw in [90, 270]
         ]
@@ -39,8 +45,8 @@ SHAPES = [
         f'reclined_fizzler_x{i}',
         f'blocks/reclined_{i}_ref',
         [
-            (Vec(0, -128*r, +48), Vec(180, 90, 0)),
-            (Vec(0, -128*l, -48), Vec(180, 270, 0)),
+            (Vec(0, -128*r, +48), Angle(180, 90, 0)),
+            (Vec(0, -128*l, -48), Angle(180, 270, 0)),
         ]
     ) for i, l, r in [
         (1, 0, 0),
@@ -71,7 +77,7 @@ def test_vmf() -> None:
             ent = vmf.create_ent(
                 'prop_static',
                 model='models/props_map_editor/fizzler.mdl',
-                origin=pos.rotate(*angle),
+                origin=pos @ angle,
                 angles=angle,
             )
             ent.visgroup_ids.add(group.id)
@@ -83,6 +89,7 @@ def test_vmf() -> None:
 
 def make_anim(
     root: Bone,
+    sty_ang: Angle,
     new_bones: Dict[Tuple[float, float, float], Dict[Bone, Bone]],
     name: str,
     animation: Dict[int, List[BoneFrame]],
@@ -100,21 +107,18 @@ def make_anim(
     # Do the animation
     for frame_num, frames in animation.items():
         mesh.animation[frame_num] = new_frames = [
-            BoneFrame(root, Vec(), Vec()),
+            BoneFrame(root, Vec(), Angle()),
         ]
-        for (pitch, yaw, roll), bone_map in new_bones.items():
-            new_frames.append(BoneFrame(
-                bone_map[root], Vec(),
-                Vec(radians(pitch), radians(yaw), radians(roll)),
-            ))
+        for angle, bone_map in new_bones.items():
+            new_frames.append(BoneFrame(bone_map[root], Vec(), Angle(angle)))
             for frame in frames:
-                new_frames.append(BoneFrame(bone_map[frame.bone], frame.position, Vec()))
+                new_frames.append(BoneFrame(bone_map[frame.bone], frame.position, frame.rotation))
 
     with open(name + '.smd', 'wb') as f:
         mesh.export(f)
 
 
-def generate(style: str, field_tex: str, shape: Shape) -> None:
+def generate(style: str, sty_ang: Angle, field_tex: str, shape: Shape) -> str:
     """Generate a specific model."""
     # Load the models we use.
     emitter = load_model(style + '_fizz_ref.smd')
@@ -139,6 +143,7 @@ def generate(style: str, field_tex: str, shape: Shape) -> None:
     ind = 1
     # Generate the duplicate sets of bones.
     for pos, angles in shape.points:
+        angles = sty_ang @ angles
         if angles.as_tuple() in new_bones:
             continue
 
@@ -161,17 +166,18 @@ def generate(style: str, field_tex: str, shape: Shape) -> None:
             ref.bones[bone.name] = bone
 
         # And copy over their start pose.
-        ref.animation[0].append(BoneFrame(local_root, Vec(), Vec()))
+        ref.animation[0].append(BoneFrame(local_root, Vec(), Angle()))
         for frame in emitter.animation[0]:
             ref.animation[0].append(BoneFrame(bones[frame.bone], frame.position, frame.rotation))
 
     # Now, place each emitter.
     for pos, angles in shape.points:
+        angles = sty_ang @ angles
         bones = new_bones[angles.as_tuple()]
         for tri in emitter.triangles:
             ref.triangles.append(Triangle(tri.mat, *[
                 Vertex(
-                    vert.pos + pos,
+                    (vert.pos @ sty_ang) + pos,
                     vert.norm,
                     vert.tex_u,
                     vert.tex_v,
@@ -184,15 +190,16 @@ def generate(style: str, field_tex: str, shape: Shape) -> None:
 
     folder = f'generated/{style}/'
 
-    make_anim(root, new_bones, f'{folder}{shape.name}_open', anim_open.animation)
-    make_anim(root, new_bones, f'{folder}{shape.name}_close', anim_close.animation)
-    make_anim(root, new_bones, f'{folder}{shape.name}_idle', {0: anim_open.animation[0]})
+    make_anim(root, sty_ang, new_bones, f'{folder}{shape.name}_open', anim_open.animation)
+    make_anim(root, sty_ang, new_bones, f'{folder}{shape.name}_close', anim_close.animation)
+    make_anim(root, sty_ang, new_bones, f'{folder}{shape.name}_idle', {0: anim_open.animation[0]})
 
     print(f' - {folder}{shape.name}_ref...', flush=True)
     with open(f'{folder}{shape.name}_ref.smd', 'wb') as f:
         ref.export(f)
 
-    with open(f'{folder}{shape.name}.qc', 'w') as f:
+    qc_file = f'{folder}{shape.name}.qc'
+    with open(qc_file, 'w') as f:
         f.write(f'$modelname "props_map_editor/BEE2/{style}/{shape.name}.mdl"\n')
         f.write('$BodyGroup "Body" {\n\tstudio "%_ref.smd"\n}\n'.replace('%', shape.name))
         f.write('$cdmaterials "models/props_map_editor/" "BEE2/models/props_map_editor/"\n')
@@ -201,15 +208,29 @@ $sequence "idle" "%_idle.smd"
 $sequence "explodeOut" "%_open.smd"
 $sequence "explodeIn" "%_close.smd"
 '''.replace('%', shape.name))
+    return qc_file
 
 
 if __name__ == '__main__':
     # test_vmf()
     import logging
     logging.basicConfig()
+    print('Starting')
     futures = []
-    with ProcessPoolExecutor() as exc:
+    with ThreadPoolExecutor() as exc:
         for cur_style in STYLES:
             for cur_shape in SHAPES:
+                print('Submit: ', cur_style, cur_shape)
                 futures.append(exc.submit(generate, *cur_style, cur_shape))
                 # generate(*cur_style, cur_shape)
+    print('Complete!')
+
+    if P2_LOC is not None:
+        for fut in futures:
+            subprocess.call([
+                P2_LOC + '/bin/studiomdl.exe',
+                '-game',
+                P2_LOC + '/bee2_dev/',
+                '-nop4',
+                fut.result(),
+            ])
