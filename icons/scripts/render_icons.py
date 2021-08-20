0194ff7b428fa0a62a37b94ac44d7f043c6d6d12
Konclan <classicblazes@gmail.com>
2021-08-20 02:10:51 -0500

    Update script import models and materials from list

--- icons/scripts/render_icons.py
+++ icons/scripts/render_icons.py
@@ -1,40 +1,38 @@
 import bpy
-import os
 from math import radians
 
-context = bpy.context
+blendPath = bpy.path.abspath("//")
+modelPath = blendPath + "models/"
+materialPath = blendPath + "materials/"
 
-models_path = "//models/"
-materials_path = "//materials/"
-render_path = "//render/"
+def appendObject(object = "", type = "", blendFile = ""):
+    
+    directory = blendFile + type
 
-# floor_models = ["bretling.obj", "bretling.obj"]
-wall_models = ["pellets/pellet_catcher.qc", "pellets/pellet_catcher_timed.qc", "pellets/pellet_launcher.qc", "pellets/pellet_launcher_inf.qc"]
+    bpy.ops.wm.append(
+        filepath=directory + object, 
+        filename=object,
+        directory=directory)
 
-#create a scene
-scene = bpy.data.scenes.new("Scene")
-camera_data = bpy.data.cameras.new("Camera")
+if not bpy.data.node_groups.get("PM Shader"):
+    appendObject("PM Shader", "\\NodeTree\\", blendPath +"pmshader.blend")
 
-camera = bpy.data.objects.new("Camera", camera_data)
-camera.location = (-2.0, 3.0, 3.0)
-camera.rotation_euler = ([radians(a) for a in (422.0, 0.0, 149)])
-scene.objects.link(camera)
+def importObjects(files = []):
 
-# do the same for lights etc
-scene.update()
+    for file in files:
+        bpy.ops.import_scene.smd(filepath=modelPath + file + ".smd", append="NEW_ARMATURE")
+        
+        vObject = bpy.context.active_object.children[0]
+        
+        bpy.context.view_layer.objects.active = vObject
+        
+        for modifier in vObject.modifiers:
+            bpy.ops.object.modifier_apply(modifier=modifier.name)
+        
+        objs = bpy.data.objects
+        objs.remove(objs[vObject.parent.name], do_unlink=True)
+        
+        vObject.active_material.use_nodes = True
 
-for model_path in models:
-    scene.camera = camera
-    path = os.path.join(models_path, model_path)
-    # make a new scene with cam and lights linked
-    context.screen.scene = scene
-    bpy.ops.scene.new(type='LINK_OBJECTS')
-    context.scene.name = model_path
-    cams = [c for c in context.scene.objects if c.type == 'CAMERA']
-    #import model
-    bpy.ops.import_scene.obj(filepath=path, axis_forward='-Z', axis_up='Y', filter_glob="*.obj;*.mtl")
-    for c in cams:
-        context.scene.camera = c                                    
-        print("Render ", model_path, context.scene.name, c.name)
-        context.scene.render.filepath = "somepathmadeupfrommodelname"
-        bpy.ops.render.render(write_still=True)
+objectsList = ["pellets/pellet_catcher_reference"]
+importObjects(objectsList)
